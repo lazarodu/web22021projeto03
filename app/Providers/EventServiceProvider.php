cd1f94da432ca518710dc62ef63c885809a41103
lazarodu <lazarodu@gmail.com>
2022-01-10 08:43:26 -0300

    Aula do dia 10/01/2021

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -9,24 +11,27 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+  /**
+   * The event listener mappings for the application.
+   *
+   * @var array
+   */
+  protected $listen = [
+    Registered::class => [
+      SendEmailVerificationNotification::class,
+    ],
+  ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  // Evento - algo que aconteceu - dados do evento
+  // Ouvinte (Listener) - É chamado de acordo com uma situação, sua ação é executada
+
+  /**
+   * Register any events for your application.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    User::observe(UserObserver::class);
+  }
 }
